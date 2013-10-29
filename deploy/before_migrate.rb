4c19098f8409af7e862cae8daba8c5def3d5ffe8
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-10-30 01:45:16 +0200

    Update before_migrate.rb

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -2,13 +2,21 @@
 # This file is a ruby script, run in the context of a chef 'deploy' action.
 # It can access all resources available to the Chef recipe.
 
+Chef::Log.debug("before_migrate.rb is running")
+
 node[:deploy].each do |app_name, deploy|
+  Chef::Log.debug("Looking at app #{app_name}")
   next unless deploy[:database]
+  
+  Chef::Log.debug("execute mysql-create-database... resource declaring now.")
   
   execute "mysql-create-database in database #{deploy[:database][:database]}" do
     command "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} -e'CREATE DATABASE IF NOT EXISTS #{deploy[:database][:database]}'"
     not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} -e'SHOW DATABASES' | grep #{deploy[:database][:database]}"
-  end
+    action :nothing
+  end.run_action(:run)
+  
+  Chef::Log.debug("execute mysql-create-table... resource declaring now.")
 
   execute "mysql-create-table in database #{deploy[:database][:database]}" do
     command "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'CREATE TABLE #{node[:phpapp][:dbtable]}(
@@ -18,9 +26,11 @@ node[:deploy].each do |app_name, deploy|
     PRIMARY KEY (id)
   )'"
     not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:phpapp][:dbtable]}"
-    action :run
-  end
+    action :nothing
+  end.run_command(:run)
 
+
+  Chef::Log.debug("script install_composer... resource declaring now.")
   # In this example, the following code block directly executes a resource from
   # a public URL. Please, don't do this in your code. It's a gaping security hole.
   # Instead, consider creating a mirror of the public resource.
@@ -32,8 +42,10 @@ node[:deploy].each do |app_name, deploy|
     curl -s https://getcomposer.org/installer | php
     php composer.phar install
     EOH
-  end
-
+    action :nothing
+  end.run_command(:run)
+  
+  Chef::Log.debug("template #{deploy[:deploy_to]}/current/db-connect.php resource declaring now.")
   template "#{deploy[:deploy_to]}/current/db-connect.php" do
     source "db-connect.php.erb"
     cookbook "mcw_deploy"
@@ -57,5 +69,6 @@ node[:deploy].each do |app_name, deploy|
    only_if do
      File.directory?("#{deploy[:deploy_to]}/current")
    end
-  end
+   action :nothing
+  end.run_command(:create)
 end
