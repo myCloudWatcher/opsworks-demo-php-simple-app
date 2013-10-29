3030d119ec488107a73f7ecc8272fc2fd6a64a27
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-10-30 00:23:30 +0200

    Update before_migrate.rb
    create database and table in DB

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -2,4 +2,22 @@
 # This file is a ruby script, run in the context of a chef 'deploy' action.
 # It can access all resources available to the Chef recipe.
 
-Chef::Log.debug("before_migrate.rb: node is #{node.inspect}")
+node[:deploy].each do |app_name, deploy|
+  next if !deploy[:database]
+  
+  execute "mysql-create-database in database #{deploy[:database][:database]}" do
+    command "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} -e'CREATE DATABASE IF NOT EXISTS #{deploy[:database][:database]}'"
+    not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} -e'SHOW DATABASES' | grep #{deploy[:database][:database]}"
+  end
+
+  execute "mysql-create-table in database #{deploy[:database][:database]}" do
+    command "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'CREATE TABLE #{node[:phpapp][:dbtable]}(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    author VARCHAR(63) NOT NULL,
+    message TEXT,
+    PRIMARY KEY (id)
+  )'"
+    not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:phpapp][:dbtable]}"
+    action :run
+  end
+end
