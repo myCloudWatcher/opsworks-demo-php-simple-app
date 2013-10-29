c529c0ff3b11f86712cc337774f10c2ffbad1803
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-10-30 00:38:11 +0200

    Update before_migrate.rb
    Initialize app dependencies and set up db-connect.php for the app.

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -3,7 +3,7 @@
 # It can access all resources available to the Chef recipe.
 
 node[:deploy].each do |app_name, deploy|
-  next if !deploy[:database]
+  next unless deploy[:database]
   
   execute "mysql-create-database in database #{deploy[:database][:database]}" do
     command "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} -e'CREATE DATABASE IF NOT EXISTS #{deploy[:database][:database]}'"
@@ -19,5 +19,43 @@ node[:deploy].each do |app_name, deploy|
   )'"
     not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:phpapp][:dbtable]}"
     action :run
+  end
+
+  # In this example, the following code block directly executes a resource from
+  # a public URL. Please, don't do this in your code. It's a gaping security hole.
+  # Instead, consider creating a mirror of the public resource.
+  script "install_composer for app #{app_name}" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    curl -s https://getcomposer.org/installer | php
+    php composer.phar install
+    EOH
+  end
+
+  template "#{deploy[:deploy_to]}/current/db-connect.php" do
+    source "db-connect.php.erb"
+    cookbook "mcw_deploy"
+    mode 0660
+    group deploy[:group]
+
+    if platform?("ubuntu")
+      owner "www-data"
+    elsif platform?("amazon")   
+      owner "apache"
+    end
+
+    variables(
+      :host =>     (deploy[:database][:host] rescue nil),
+      :user =>     (deploy[:database][:username] rescue nil),
+      :password => (deploy[:database][:password] rescue nil),
+      :db =>       (deploy[:database][:database] rescue nil),
+      :table =>    (node[:phpapp][:dbtable] rescue nil)
+    )
+
+   only_if do
+     File.directory?("#{deploy[:deploy_to]}/current")
+   end
   end
 end
