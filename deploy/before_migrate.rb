4adbddf063aa5d5fd17862e0f0bb766057cea463
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-10-30 03:46:39 +0200

    Update before_migrate.rb

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -8,8 +8,6 @@ node[:deploy].each do |app_name, deploy|
   Chef::Log.debug("Looking at app #{app_name}")
   next unless deploy[:database]
   
-  Chef::Log.debug("execute mysql-create-database... resource declaring now.")
-  
   execute "mysql-create-database #{deploy[:database][:database]}" do
     command "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p'#{deploy[:database][:password]}' -e 'CREATE DATABASE IF NOT EXISTS #{deploy[:database][:database]}'"
     not_if "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p'#{deploy[:database][:password]}' -e 'SHOW DATABASES' | fgrep #{deploy[:database][:database]}"
@@ -25,41 +23,4 @@ node[:deploy].each do |app_name, deploy|
     not_if "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p'#{deploy[:database][:password]}' #{deploy[:database][:database]} -e 'SHOW TABLES' | grep #{deploy[:database][:table]}"
   end
 
-  # In this example, the following code block directly executes a resource from
-  # a public URL. Please, don't do this in your code. It's a gaping security hole.
-  # Instead, consider creating a mirror of the public resource.
-  script "install_composer for app #{app_name}" do
-    interpreter "bash"
-    user "root"
-    cwd "#{deploy[:deploy_to]}/current"
-    code <<-EOH
-    curl -s https://getcomposer.org/installer | php
-    php composer.phar install
-    EOH
-  end
-  
-  template "#{deploy[:deploy_to]}/current/db-connect.php" do
-    source "db-connect.php.erb"
-    cookbook "mcw_deploy" # assumption: this cookbook will be in the custom cookbook repo
-    mode 0660
-    group deploy[:group]
-
-    if platform?("ubuntu")
-      owner "www-data"
-    elsif platform?("amazon")   
-      owner "apache"
-    end
-
-    variables(
-      :host =>     (deploy[:database][:host] rescue nil),
-      :user =>     (deploy[:database][:username] rescue nil),
-      :password => (deploy[:database][:password] rescue nil),
-      :db =>       (deploy[:database][:database] rescue nil),
-      :table =>    (deploy[:database][:table] rescue nil)
-    )
-
-   only_if do
-     File.directory?("#{deploy[:deploy_to]}/current")
-   end
-  end
 end
