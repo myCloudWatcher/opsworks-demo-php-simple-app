128658b41ade4f94433dc1e3aab6a9075bed3080
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-10-30 03:50:22 +0200

    Update before_restart.rb
    Move the app setup step to before_restart.rb because the "current" link is set in the symlink step (just prior to the restart).

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -4,3 +4,44 @@
 # It can access all resources available to the Chef recipe.
 
 Chef::Log.debug("before_restart.rb is running.")
+
+node[:deploy].each do |app_name, deploy|
+  
+  # In this example, the following code block directly executes a resource from
+  # a public URL. Please, don't do this in your code. It's a gaping security hole.
+  # Instead, consider creating a mirror of the public resource.
+  script "install_composer for app #{app_name}" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    curl -s https://getcomposer.org/installer | php
+    php composer.phar install
+    EOH
+  end
+  
+  template "#{deploy[:deploy_to]}/current/db-connect.php" do
+    source "db-connect.php.erb"
+    cookbook "mcw_deploy" # assumption: this cookbook will be in the custom cookbook repo
+    mode 0660
+    group deploy[:group]
+
+    if platform?("ubuntu")
+      owner "www-data"
+    elsif platform?("amazon")   
+      owner "apache"
+    end
+
+    variables(
+      :host =>     (deploy[:database][:host] rescue nil),
+      :user =>     (deploy[:database][:username] rescue nil),
+      :password => (deploy[:database][:password] rescue nil),
+      :db =>       (deploy[:database][:database] rescue nil),
+      :table =>    (deploy[:database][:table] rescue nil)
+    )
+
+    only_if do
+      File.directory?("#{deploy[:deploy_to]}/current")
+  end
+  
+end
