f273fe6df8eb1d300bdd3462a7f364aeb9377351
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-10-30 02:13:58 +0200

    Update after_restart.rb

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -3,4 +3,4 @@
 # This file is a ruby script, run in the context of a chef 'deploy' action.
 # It can access all resources available to the Chef recipe.
 
-Chef::Log.debug("after_restart.rb: node is #{node.inspect}")
+Chef::Log.debug("after_restart.rb is running.")
