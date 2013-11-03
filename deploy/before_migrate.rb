d8a4d128d65e7a687353b30d416983c1d0ac3bf2
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-11-03 22:14:05 +0200

    change variable name to avoid weird infinite loop
    and restore checking for necessary attributes

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -4,28 +4,28 @@
 
 Chef::Log.debug("before_migrate.rb is running")
 
-node[:deploy].each do |app_name, deploy|
+node[:deploy].each do |app_name, deploy_item|
   Chef::Log.debug("Looking at app #{app_name}")
-  next unless deploy[:database]
-#  next unless deploy_item[:database][:host]
-#  next unless deploy_item[:database][:username]
-#  next unless deploy_item[:database][:password]
-#  next unless deploy_item[:database][:database]
-#  next unless deploy_item[:database][:table]
+  next unless deploy_item[:database]
+  next unless deploy_item[:database][:host]
+  next unless deploy_item[:database][:username]
+  next unless deploy_item[:database][:password]
+  next unless deploy_item[:database][:database]
+  next unless deploy_item[:database][:table]
   
-  execute "mysql-create-database #{deploy[:database][:database]}" do
-    command "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p'#{deploy[:database][:password]}' -e 'CREATE DATABASE IF NOT EXISTS #{deploy[:database][:database]}'"
-    not_if "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p'#{deploy[:database][:password]}' -e 'SHOW DATABASES' | fgrep #{deploy[:database][:database]}"
+  execute "mysql-create-database #{deploy_item[:database][:database]}" do
+    command "/usr/bin/mysql -h #{deploy_item[:database][:host]} -u #{deploy_item[:database][:username]} -p'#{deploy_item[:database][:password]}' -e 'CREATE DATABASE IF NOT EXISTS #{deploy_item[:database][:database]}'"
+    not_if "/usr/bin/mysql -h #{deploy_item[:database][:host]} -u #{deploy_item[:database][:username]} -p'#{deploy_item[:database][:password]}' -e 'SHOW DATABASES' | fgrep #{deploy_item[:database][:database]}"
   end
 
-  execute "mysql-create-table #{deploy[:database][:database]}" do
-    command "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p'#{deploy[:database][:password]}' #{deploy[:database][:database]} -e 'CREATE TABLE #{deploy[:database][:table]}(
+  execute "mysql-create-table #{deploy_item[:database][:database]}" do
+    command "/usr/bin/mysql -h #{deploy_item[:database][:host]} -u #{deploy_item[:database][:username]} -p'#{deploy_item[:database][:password]}' #{deploy_item[:database][:database]} -e 'CREATE TABLE #{deploy_item[:database][:table]}(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     author VARCHAR(63) NOT NULL,
     message TEXT,
     PRIMARY KEY (id)
   )'"
-    not_if "/usr/bin/mysql -h #{deploy[:database][:host]} -u #{deploy[:database][:username]} -p'#{deploy[:database][:password]}' #{deploy[:database][:database]} -e 'SHOW TABLES' | grep #{deploy[:database][:table]}"
+    not_if "/usr/bin/mysql -h #{deploy_item[:database][:host]} -u #{deploy_item[:database][:username]} -p'#{deploy_item[:database][:password]}' #{deploy_item[:database][:database]} -e 'SHOW TABLES' | grep #{deploy_item[:database][:table]}"
   end
 
 end
